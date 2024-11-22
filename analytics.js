e01429c7d39b2d6f71e8092e79607f49db4c97a1
nethanielscaglione <zsweettop@gmail.com>
2024-11-22 11:20:43 +0700

    Files Update

---
+++ analytics.js
@@ -0,0 +1,11 @@
+// analytics.js
+var script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-M7BLN9VCR0';
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-M7BLN9VCR0');
